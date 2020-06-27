95010a8f91bef7d5190e3a2b132000654066bad2
dmitrii <ffklffkl@gmail.com>
2020-06-27 16:20:02 +0300

    feature: ability to subscribe on partial changes

--- src/store.spec.ts
+++ src/store.spec.ts
@@ -1,3 +1,4 @@
+import {take} from 'rxjs/operators';
 import {Store} from './store';
 
 interface OptionalSublevel {
@@ -484,6 +485,73 @@ describe('Store', () => {
                 'fourthSublevel2'
             );
             expect(store.state).toEqual(expectedState);
+        });
+
+        it('should get values on "twoSublevels-firstSublevel1-secondSublevel1"', done => {
+            const result = [];
+            store.onChanges('twoSublevels', 'firstSublevel1', 'secondSublevel1', 'value1')
+                .pipe(take(2))
+                .subscribe({
+                    next(val) {
+                        result.push(val);
+                    },
+                    complete() {
+                        expect(result).toEqual([
+                            'twoSublevels-firstSublevel1-secondSublevel1: value1',
+                            'twoSublevels-firstSublevel1-secondSublevel1: value1 patched'
+                        ]);
+                        done();
+                    }
+                });
+
+            store.patchState(
+                'twoSublevels-firstSublevel1-secondSublevel1: value1 patched',
+                'twoSublevels',
+                'firstSublevel1',
+                'secondSublevel1',
+                'value1'
+            );
+        });
+
+        it('should skip unchanged value on "oneSublevel"', done => {
+            const result = [];
+            store.onChanges('oneSublevel', 'value1')
+                .pipe(take(2))
+                .subscribe({
+                    next(val) {
+                        result.push(val);
+                    },
+                    complete() {
+                        expect(result).toEqual([
+                            'oneSublevel: value1',
+                            'oneSublevel: value1 patched'
+                        ]);
+                        done();
+                    }
+                });
+
+            store.patchState('oneSublevel: value1', 'oneSublevel', 'value1');
+            store.patchState('oneSublevel: value1 patched', 'oneSublevel', 'value1');
+        });
+
+        it('should get null value on "nullableTwoSublevels-firstSublevel1"', done => {
+            const result = [];
+            store.onChanges('nullableTwoSublevels', 'firstSublevel1')
+                .pipe(take(2))
+                .subscribe({
+                    next(val) {
+                        result.push(val);
+                    },
+                    complete() {
+                        expect(result).toEqual([
+                            null,
+                            {secondSublevel1: {value1: ''}}
+                        ]);
+                        done();
+                    }
+                });
+
+            store.patchState({secondSublevel1: {value1: ''}}, 'nullableTwoSublevels', 'firstSublevel1');
         });
     });
 });
--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
-import {Observable, BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {distinctUntilChanged, map} from 'rxjs/operators';
 import {Object, Tuple} from 'ts-toolbelt';
 
 type Index = string | number | symbol;
@@ -22,7 +23,7 @@ export class Store<S extends object> {
 
     // prettier-ignore
     patchState<
-        P1 extends keyof Object.Path<S, []>,
+        P1 extends keyof Object.Path<S, []>
     >(value: Object.Path<S, [P1]>, part1: P1): void;
     // prettier-ignore
     patchState<
@@ -125,6 +126,111 @@ export class Store<S extends object> {
             return;
         }
         this.setState(this.getUpdatedState(value, this.state, path));
+    }
+
+    // prettier-ignore
+    onChanges<P1 extends keyof Object.Path<S, []>>(part1: P1): Observable<Object.Path<S, [P1]>>;
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>
+    >(part1: P1, part2: P2): Observable<Object.Path<S, [P1, P2]>>;
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>
+    >(part1: P1, part2: P2, part3: P3): Observable<Object.Path<S, [P1, P2, P3]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4): Observable<Object.Path<S, [P1, P2, P3, P4]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5): Observable<Object.Path<S, [P1, P2, P3, P4, P5]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>,
+        P6 extends keyof Object.Path<S, [P1, P2, P3, P4, P5]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5, part6: P6): Observable<Object.Path<S, [P1, P2, P3, P4, P5, P6]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>,
+        P6 extends keyof Object.Path<S, [P1, P2, P3, P4, P5]>,
+        P7 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5, part6: P6, part7: P7): Observable<Object.Path<S, [P1, P2, P3, P4, P5, P6, P7]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>,
+        P6 extends keyof Object.Path<S, [P1, P2, P3, P4, P5]>,
+        P7 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6]>,
+        P8 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5, part6: P6, part7: P7, part8: P8): Observable<Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>,
+        P6 extends keyof Object.Path<S, [P1, P2, P3, P4, P5]>,
+        P7 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6]>,
+        P8 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7]>,
+        P9 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5, part6: P6, part7: P7, part8: P8, part9: P9): Observable<Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8, P9]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>,
+        P6 extends keyof Object.Path<S, [P1, P2, P3, P4, P5]>,
+        P7 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6]>,
+        P8 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7]>,
+        P9 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8]>,
+        P10 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8, P9]>
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5, part6: P6, part7: P7, part8: P8, part9: P9, part10: P10): Observable<Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10]>>
+    // prettier-ignore
+    onChanges<
+        P1 extends keyof Object.Path<S, []>,
+        P2 extends keyof Object.Path<S, [P1]>,
+        P3 extends keyof Object.Path<S, [P1, P2]>,
+        P4 extends keyof Object.Path<S, [P1, P2, P3]>,
+        P5 extends keyof Object.Path<S, [P1, P2, P3, P4]>,
+        P6 extends keyof Object.Path<S, [P1, P2, P3, P4, P5]>,
+        P7 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6]>,
+        P8 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7]>,
+        P9 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8]>,
+        P10 extends keyof Object.Path<S, [P1, P2, P3, P4, P5, P6, P7, P8, P9]>,
+        R extends Index[]
+    >(part1: P1, part2: P2, part3: P3, part4: P4, part5: P5, part6: P6, part7: P7, part8: P8, part9: P9, part10: P10, ...rest: R): Observable<Object.Path<S, Tuple.Concat<[P1, P2, P3, P4, P5, P6, P7, P8, P9, P10], R>>>
+
+    onChanges(...path: Index[]) {
+        return this.state$.pipe(
+            map(state => path.reduce((result, part) => result?.[part], state)),
+            distinctUntilChanged()
+        )
     }
 
     private getUpdatedState(value: any, stateSubtree: any, path: Index[]): any {
